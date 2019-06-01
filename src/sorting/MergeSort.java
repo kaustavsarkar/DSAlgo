eb69d84e00d0eb854c29387689645b4b9286138e
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2019-06-01 21:25:16 +0530

    Adding merge sort algo. Makinf merge algo concise

--- src/sorting/MergeSort.java
+++ src/sorting/MergeSort.java
@@ -7,51 +7,46 @@ public class MergeSort {
     public static void main(String[] args) {
 
         MergeSort merge = new MergeSort();
-        int arr[] = { 3, 8, 9, 1, 5, 7  };
+        int arr[] = { 3, 48, 29, 1, 15, 7 , 77};
         int low = 0;
         int high = arr.length - 1;
-        int mid = (low + high + 1) / 2;
-        merge.merge(arr, low, high, mid);
+        merge.divide(arr, high, low);
+        System.out.println(Arrays.toString(arr));
+    }
 
+    public void divide(int[] arr, int high, int low) {
+        if (low < high) {
+            int middle = (low + high + 1) / 2;
+            divide(arr, middle - 1, low);// left side
+            divide(arr, high, middle);// right side
+            merge(arr, low, high, middle);
+        }
+    }
+
+    public void merge(int[] arr, int l, int h, int m) {
+        int temp[] = new int[h - l + 1];
+        for (int i = 0; i < temp.length; i++) { // copying array to temp array
+            temp[i] = arr[l + i];
+        }
+        int low = 0;
+        int high = temp.length;
+        int mid = (low + high) / 2;
+        int middle = mid;
+        int counter = 0;
+        while (low < middle && mid < high) { // loop till low comes to mid || mid reaches high
+            if (temp[low] < temp[mid]) {
+                arr[l + counter++] = temp[low++];
+            } else {
+                arr[l + counter++] = temp[mid++];
+            }
+        }
+        while (low < middle) { //Add anything remaining below middle
+            arr[l + counter++] = temp[low++];
+        }
+        while (mid < high) { // Add anything remaining post middle
+            arr[l + counter++] = temp[mid++];
+        }
     }
 
     // 0000,1,5,7,3,8,9,00000
-    public void merge(int arr[], int l, int h, int m) {
-        if (h <= l || h <= m) {
-            return;
-        }
-        // Copy Array to temp
-        int temp[] = new int[h - l + 1];
-        for (int i = 0; i < temp.length; i++) {
-            temp[i] = arr[l + i];
-        }
-
-        int low = 0;
-        int high = temp.length-1;
-        int mid = (low + high + 1) / 2;
-        int counter = 0;
-        //Compare both sides of array
-        while (low < m && mid < h) {
-            if (temp[low] < temp[mid]) {
-                arr[l+counter] = temp[low];
-                low++;
-            } else {
-                arr[l+counter] = temp[mid];
-                mid++;
-            }
-            counter++;
-        }
-        //Add if remaining values in left
-        while(low < m) {
-            arr[l+counter] = temp[low];
-            low++;
-            counter++;
-        }
-        //Add if remaining values in right
-        while(mid < h) {
-            arr[l+counter] = temp[mid];
-            mid++;
-            counter++;
-        }
-    }
 }
